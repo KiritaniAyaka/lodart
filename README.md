52fe2499926f273cc3abb7543a1590da79c2b633
Ayaka <KiritaniAyaka@users.noreply.github.com>
2024-01-26 13:33:43 +0800

    chore: change descriptions

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Lodart
 
-Lodart is a dart utility library like lodash.
+Lodart is a Dart utility library like lodash.
+
+The goal of lodart is NOT to implement all methods in lodash because of the difference between Dart and JavaScript.
+Lodart aims to implement utilities to make Dart easier to use and keep similar naming rules to lodash.
 
 ## Features
 
--- lib/lodart.dart
+++ lib/lodart.dart
@@ -1,4 +1,7 @@
-/// Lodart is a dart utility library like lodash.
+/// Lodart is a Dart utility library like lodash.
+///
+/// The goal of lodart is NOT to implement all methods in lodash because of the difference between Dart and JavaScript.
+/// Lodart aims to implement utilities to make Dart easier to use and keep similar naming rules to lodash.
 library;
 
 export 'src/collections/group_by.dart';
